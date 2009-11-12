6cfeedac0f46e8115b9dfed190920957e35498b3
macournoyer <macournoyer@gmail.com>
2009-11-12 14:42:11 -0500

    Add script to connect a process to a room.

---
+++ lib/talker/cli.rb
@@ -0,0 +1,16 @@
+class Talker
+  class CLI
+    def self.load_token
+      token_path = File.join(ENV['HOME'], '.talker')
+      
+      unless File.file?(token_path)
+        abort <<-EOS
+      Place your Talker Token in #{token_path}. You can find your
+      token in https://myaccount.talkerapp.com/settings
+      EOS
+      end
+      
+      File.read(token_path).strip
+    end
+  end
+end
